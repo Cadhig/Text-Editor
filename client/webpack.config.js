49644f71fe9ea8668f1c38aeee164066c1d572a9
Cadence K <cadencehiggins@gmail.com>
2024-07-30 13:17:04 -0500

    configured webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const JATE = require('./manifest.json')
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -13,17 +14,21 @@ module.exports = () => {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
     plugins: [
-      
+      new HtmlWebpackPlugin({ template: './index.html' }),
     ],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js',
+    },
 
     module: {
       rules: [
-        
+        { test: /\.txt$/, use: 'raw-loader' },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
       ],
     },
   };
