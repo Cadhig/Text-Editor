df8e5f4855569fff96fa62a717c588f38206465b
Cadence K <cadencehiggins@gmail.com>
2024-07-31 15:17:57 -0500

    final push

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,17 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  await db.put('jate', content, db.keyPath)
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  openDB('jate', 1, {
+    upgrade(db) {
+      db.getAll('jate')
+    }
+  })
+};
 
 initdb();
