9a9e3d48807ddc7e22cac9dd63e9ec72e2fbe792
Cadence Higgins <160413853+Cadhig@users.noreply.github.com>
2024-08-05 18:19:38 -0500

    Delete client/dist directory

--- client/dist/install.bundle.js
+++
@@ -1,1 +0,0 @@
-(()=>{const t=document.getElementById("buttonInstall");let n=null;window.addEventListener("beforeinstallprompt",(t=>{t.preventDefault(),n=t})),t.addEventListener("click",(async()=>{if(!n)return;const t=await n.prompt();console.log(`Install prompt was: ${t.outcome}`),n=null})),window.addEventListener("appinstalled",(t=>{n=null}))})();
